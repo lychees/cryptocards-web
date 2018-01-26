32bd844b82a1dd5413bf51ecd91b2577d09037b2
KJlmfe <kjlmfe@gmail.com>
2018-01-26 17:06:11 +0800

    chore(package): add && use vue-config module
    Put all configuations in config/index.js file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import vueConfig from 'vue-config';
+import config from '@/config';
 import 'normalize.css';
 import App from './App';
 import router from './router';
@@ -8,6 +10,8 @@ import store from './store';
 import i18n from './i18n';
 
 Vue.config.productionTip = false;
+
+Vue.use(vueConfig, config);
 
 const app = new Vue({
   el: '#app',
