dae95033312446f34300d8663720e3563e5b0152
KJlmfe <kjlmfe@gmail.com>
2018-02-05 16:43:02 +0800

    chore(package): add && use element-ui module

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import vueConfig from 'vue-config';
 import config from '@/config';
 import 'normalize.css';
@@ -12,6 +14,7 @@ import i18n from './i18n';
 Vue.config.productionTip = false;
 
 Vue.use(vueConfig, config);
+Vue.use(ElementUI);
 
 const app = new Vue({
   el: '#app',
