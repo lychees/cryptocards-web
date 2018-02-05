e01092dbb31accd5c135a99eb6b3d56cfa0c1d3d
KJlmfe <kjlmfe@gmail.com>
2018-02-05 18:48:42 +0800

    chore(static): add && use style.css
    style.css is copied from others for the internal demo, it will be removed in production

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@
     </main>
     <Footer />
   </div>
-
 </template>
 
 <script>
@@ -40,12 +39,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
