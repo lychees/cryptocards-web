3550f05a9fdd50bdb4ee9b42212cfc54671c97ef
KJlmfe <kjlmfe@gmail.com>
2018-02-06 10:33:38 +0800

    feat(user): show a user's info and cards/cardPacks in his page

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,12 +1,64 @@
 <template>
-  <div class="UserView">
-    UserView
+  <div class="ProfilePage">
+    <div class="Container Container--lg">
+      <div class="ProfileHeader">
+        <div v-if="user"
+             class="ProfileHeader-details"><img class="ProfileHeader-image"
+               :src="user.imageUrl">
+          <div class="ProfileHeader-info">
+            <h1 class="ProfileHeader-title">{{user.nickname}}</h1>
+            <div class="ProfileHeader-wallet">
+              {{$t('Address')}}
+              <span style="font-size:0.9em">{{user.address}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <BrowseItems :filterTabs=tabList />
   </div>
 </template>
 
 <script>
+import BrowseItems from '@/components/BrowseItems';
+import api from '@/api';
+
 export default {
   name: 'UserView',
+  components: {
+    BrowseItems,
+  },
+  computed: {
+    isMe() {
+      const me = this.$store.state.me || {};
+      return me.address === this.$route.params.address;
+    },
+    tabList() {
+      return [
+        {
+          label: this.isMe ? 'My Cards' : 'His Cards',
+          type: 'Card',
+        },
+        {
+          label: this.isMe ? 'My CardPacks' : 'His CardPacks',
+          type: 'CardPack',
+        },
+      ];
+    },
+  },
+  data() {
+    return {
+      user: null,
+    };
+  },
+  methods: {
+    async loadUser() {
+      this.user = await api.getUser(this.$route.params.address);
+    },
+  },
+  created() {
+    this.loadUser();
+  },
 };
 </script>
 
